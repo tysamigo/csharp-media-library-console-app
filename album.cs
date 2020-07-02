698229f64dce83241e7f6db6aaa97145473f7b3c
Tyler Ramage <tysamigo@gmail.com>
2020-07-02 17:51:16 -0400

    add album.cs, created its class and class definition

---
+++ album.cs
@@ -0,0 +1,9 @@
+namespace project_csharp_media_library_console_app
+{
+    class Album
+    {
+        public string Title;
+        public string Artist;
+        public string YearReleased;
+    }
+}
