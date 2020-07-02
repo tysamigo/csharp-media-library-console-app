79be0c6ad41e54fb6e1e4292d4b53df0b2919320
Tyler Ramage <tysamigo@gmail.com>
2020-07-02 17:51:45 -0400

    add book.cs, add its class and class definition

---
+++ book.cs
@@ -0,0 +1,9 @@
+namespace project_csharp_media_library_console_app
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public string YearReleased;
+    }
+}
