149db6972e99d11f4b2bea726f228b1b48a49ede
Tyler Ramage <tysamigo@gmail.com>
2020-07-02 15:12:44 -0400

    Initial commit, this is just after a new console app was created, bare bones

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_csharp_media_library_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
