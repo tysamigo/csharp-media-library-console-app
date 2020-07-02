08896a266f9dc57c21292e073cb687c9f14d83f1
Tyler Ramage <tysamigo@gmail.com>
2020-07-02 17:52:09 -0400

    add movie.cs, add its class and class definition

---
+++ movie.cs
@@ -0,0 +1,9 @@
+namespace project_csharp_media_library_console_app
+{
+    class Movie
+    {
+        public string Title;
+        public string Director;
+        public string YearReleased;
+    }
+}
