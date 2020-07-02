f0fea532023b70b8050e9673872fb7025a13406d
Tyler Ramage <tysamigo@gmail.com>
2020-07-02 17:52:32 -0400

    add video-game.cs, add its class and class definition

---
+++ video-game.cs
@@ -0,0 +1,9 @@
+namespace project_csharp_media_library_console_app
+{
+    class VideoGame
+    {
+        public string Title;
+        public string Platform;
+        public string YearReleased;
+        public string Developers;
+}
