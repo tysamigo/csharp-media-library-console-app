7d93ea88b86901c6bd72d6604a4d9cb6df31fbad
Tyler Ramage <tysamigo@gmail.com>
2020-07-03 19:33:50 -0400

    Made a new album object and added code to test the LoanMedia and ReturnMedia methods in album.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace project_csharp_media_library_console_app
         static void Main(string[] args)
         {
             var album = new Album("A Place To Be Real", "Day.Din", "2009");
+            var album1 = new Album("Bad", "Michael Jackson", "1987");
             var book = new Book("The God Delusion", "Richard Dawkins", "2006");
             var movie = new Movie("Star Wars", "George Lucas", "1977");
             var videoGame = new VideoGame("Halo", "Xbox", "2001", "Bungie");
@@ -15,6 +16,18 @@ namespace project_csharp_media_library_console_app
             Console.WriteLine(book.GetDisplayText());
             Console.WriteLine(movie.GetDisplayText());
             Console.WriteLine(videoGame.GetDisplayText());
+
+            album.LoanMedia("Tyler Ramage");
+            album1.LoanMedia();
+
+            Console.WriteLine(album.GetDisplayText());
+            Console.WriteLine(album1.GetDisplayText());
+
+            album.ReturnMedia();
+            album1.ReturnMedia();
+
+            Console.WriteLine(album.GetDisplayText());
+            Console.WriteLine(album1.GetDisplayText());
         }
     }
 }
