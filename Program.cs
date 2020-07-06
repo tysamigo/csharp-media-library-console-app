406353a8970cef8d363ef3524ea982bc4d26c70e
Tyler Ramage <tysamigo@gmail.com>
2020-07-06 17:04:06 -0400

    add new objects and test

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace project_csharp_media_library_console_app
             var movie = new Movie("Star Wars", "George Lucas", "1977");
             var movie1 = new Movie("Battlestar Galactica", "Ronald D. Moore", "1978");
             var videoGame = new VideoGame("Halo", "Xbox", "2001", "Bungie");            
-            var VideoGame1 = new VideoGame("Call of Duty", "Xbox", "2003", "Activision");
+            var videoGame1 = new VideoGame("Call of Duty", "Xbox", "2003", "Activision");
 
             Console.WriteLine(album.GetDisplayText());
             Console.WriteLine(album1.GetDisplayText());
@@ -20,19 +20,29 @@ namespace project_csharp_media_library_console_app
             Console.WriteLine(movie.GetDisplayText());
             Console.WriteLine(movie1.GetDisplayText());
             Console.WriteLine(videoGame.GetDisplayText());
-            Console.WriteLine(VideoGame1.GetDisplayText());
+            Console.WriteLine(videoGame1.GetDisplayText());
+            Console.WriteLine(" ");
 
             album.LoanMedia("Tyler Ramage");
             album1.LoanMedia();
+            movie1.LoanMedia("Lacey Ramage");
+            videoGame1.LoanMedia("Tyler Ramage");
+
 
             Console.WriteLine(album.GetDisplayText());
             Console.WriteLine(album1.GetDisplayText());
+            Console.WriteLine(movie1.GetDisplayText());
+            Console.WriteLine(videoGame1.GetDisplayText());
 
             album.ReturnMedia();
             album1.ReturnMedia();
+            movie1.ReturnMedia();
+            videoGame1.ReturnMedia();
 
             Console.WriteLine(album.GetDisplayText());
             Console.WriteLine(album1.GetDisplayText());
+            Console.WriteLine(movie1.GetDisplayText());
+            Console.WriteLine(videoGame1.GetDisplayText());
         }
     }
 }
